5483fd8967ad941018b74c7ddb38d9bd98d34630
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-03-11 23:13:49 -0300

    Create Kind scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :kinds
+  get '/test', to: 'kinds#index'
+  root to: 'kinds#index'
 end
