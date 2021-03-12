dfb561b9d0c9eacfc1f4ba614d0c2aa10962db03
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-03-12 00:06:24 -0300

    Add variables to ERB

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,8 +1,11 @@
 class AboutController < ApplicationController
   def index
+    @company_name = 'IFPR'
   end
 
   def employees
+    @employees = %w[Diego Fritz Franz Berlin Frida]
+
     render :friends
   end
 end
