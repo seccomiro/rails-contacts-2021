229a9e87ebe1c9a346f015324749d01c3b656619
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-03-12 00:06:05 -0300

    Fix kinds route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # get 'about/index'
   get 'about', to: 'about#index'
   get 'about/employees'
-  resources :kindas
+  resources :kinds
   get 'test', to: 'kinds#index'
   root to: 'kinds#index'
 end
